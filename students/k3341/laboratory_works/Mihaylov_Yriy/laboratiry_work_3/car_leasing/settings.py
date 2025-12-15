480c0af0a55be553e247ff2e89428cc38a6858b2
MKYrii <mihailovywork@gmail.com>
2025-12-15 04:21:25 +0300

    add cors settings

--- students/k3341/laboratory_works/Mihaylov_Yriy/laboratiry_work_3/car_leasing/settings.py
+++ students/k3341/laboratory_works/Mihaylov_Yriy/laboratiry_work_3/car_leasing/settings.py
@@ -41,7 +41,19 @@ INSTALLED_APPS = [
     'djoser',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'corsheaders',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http//:localhost:8000',
+)
 
 AUTH_USER_MODEL = "leasing_app.AdminUser"
 
